1a295f64e4fadad17c81273d35a78df139d3feec
rs555 <84246573+worldofrs@users.noreply.github.com>
2022-12-26 22:52:21 +0000

    edits

--- QuizApp Foundation/pyquiz.py
+++ QuizApp Foundation/pyquiz.py
@@ -11,6 +11,9 @@ class QuizApp:
         self.greeting()
 
         # TODO: ask the user for their name
+        self.username = input("What is your name?")
+        print(f"Welcome, {self.username}!")
+        print()
 
     def greeting(self):
         print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
@@ -40,6 +43,40 @@ class QuizApp:
         # TODO: get the user's selection and act on it. This loop will
         # run until the user exits the app
         selection = ""
+        while(True):
+            selection = input("Selection? ")
+
+            if len(selection) == 0:
+                self.menu_error()
+                continue
+
+            selection = selection.capitalize()
+            
+            if selection[0] == "E":
+                self.goodbye 
+                break
+
+            elif selection[0] == "M":
+                self.menu_header()
+                continue
+
+            elif selection[0] == "L":
+                print("\Available quizzes are: ")
+                #TODO list quizzes
+                print("-----------------------\n")
+                continue
+
+            elif selection[0] == "T":
+                try: 
+                    quiznum = int(input("Quiz number: "))
+                    print(f"You have selected quiz {quiznum}")
+                    #TODO start the quiz
+
+                except: 
+                        self.menu_error
+
+            else: 
+                self.menu_error() 
 
 
     # This is the entry point to the program
